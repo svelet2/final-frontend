8cd189ab24e1bdd36394a34b1fcd2d0337552a8a
Sasha <svelet@iu.edu>
2024-05-02 16:25:11 -0400

    deliverInfopg

--- configuration.js
+++ configuration.js
@@ -1,7 +1,7 @@
 const mode = 0;
 
 const host_local = "http://localhost:8080";
-const host_remote = "https://coffee-order-svelet2.onrender.com";
+const host_remote = "https://final-svelet2.onrender.com";
 
 function getHost() {
     return (mode == 0) ? host_local : host_remote;
